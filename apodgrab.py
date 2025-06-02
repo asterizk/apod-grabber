2c9956f31dddbb86895e9f611e9b569855faf4f3
Krishen Greenwell <asterizk@mac.com>
2025-06-02 14:16:31 -0400

    Fix compile errors
    ImageDraw.multiline_textsize() has been deprecated in favor of textbbox() and multiline_textbbox() since Pillow 10.0.0 (Jul 2023)

--- apodgrab.py
+++ apodgrab.py
@@ -16,18 +16,22 @@ def text_wrap(text, font, writing, max_width, max_height):
     for word in words:
         # try putting this word in last line then measure
         lines[-1].append(word)
-        (w, h) = writing.multiline_textsize('\n'.join([' '.join(line) for line in lines]), font=font)
-        if w > max_width:  # too wide
+        bbox = writing.multiline_textbbox((0, 0), text, font=font)
+        width = bbox[2] - bbox[0]
+        height = bbox[3] - bbox[1]
+        if width > max_width:  # too wide
             # take it back out, put it on the next line, then measure again
             lines.append([lines[-1].pop()])
-            (w, h) = writing.multiline_textsize('\n'.join([' '.join(line) for line in lines]), font=font)
-            if h > max_height:  # too high now, cannot fit this word in, so take out - add ellipses
+            bbox = writing.multiline_textbbox((0, 0), text, font=font)
+            width = bbox[2] - bbox[0]
+            height = bbox[3] - bbox[1]
+            if height > max_height:  # too high now, cannot fit this word in, so take out - add ellipses
                 lines.pop()
                 # try adding ellipses to last word fitting (i.e. without a space)
                 lines[-1][-1] += '...'
                 # keep checking that this doesn't make the textbox too wide, 
                 # if so, cycle through previous words until the ellipses can fit
-                while writing.multiline_textsize('\n'.join([' '.join(line)
+                while writing.multiline_textbbox('\n'.join([' '.join(line)
                                                             for line in lines]), font=font)[0] > max_width:
                     lines[-1].pop()
                     lines[-1][-1] += '...'
